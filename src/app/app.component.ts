6804a5aed6583f580de92c91a76e479fcc8b9f29
m.digun <m.digun@alfa2b.ru>
2022-08-31 01:43:05 +0300

    adding a custom routing (for managing data practice)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  currentPage: string = 'recipes'
 
+  changingPage(pageName: string) {
+    this.currentPage = pageName
+  }
 }
